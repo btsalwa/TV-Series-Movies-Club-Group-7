7047685d6100dfc7b1344066073ff7e230784c34
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-07-15 22:14:04 +0300

    host='0.0.0.0', port='10000'

--- server/run.py
+++ server/run.py
@@ -5,5 +5,5 @@ from app.models import fetch_and_store_tmdb_data
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='10000', debug=True)
     fetch_and_store_tmdb_data()
