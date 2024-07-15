740d1a120a43ef7c5fc7a53fc80a9681086c8c0b
btsalwa <brian.tsalwa@student.moringaschool.com>
2024-07-15 23:25:07 +0300

    api working

--- server/run.py
+++ server/run.py
@@ -1,9 +1,7 @@
 from app import create_app
-from app.models import fetch_and_store_tmdb_data
 
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='10000', debug=True)
-    fetch_and_store_tmdb_data()
